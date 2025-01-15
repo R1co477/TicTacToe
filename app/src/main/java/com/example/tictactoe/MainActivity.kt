a28bf6fae0e3ea56441c023ca7528b40908cc360
Volodymyr Hladkyi <sving477@gmail.com>
2025-01-15 19:45:57 +0200

    add activity difficulty

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.tictactoe
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -14,5 +16,9 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.btnSinglePlayer.setOnClickListener {
+            val intent = Intent(this, DifficultuActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
