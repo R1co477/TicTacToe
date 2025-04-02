fc715d12965218b31374965e00480e953014b765
Volodymyr Hladkyi <sving477@gmail.com>
2025-04-02 14:11:48 +0300

    update eng localization

--- app/src/main/java/com/example/tictactoe/screens/EditProfileFragment.kt
+++ app/src/main/java/com/example/tictactoe/screens/EditProfileFragment.kt
@@ -149,7 +149,7 @@ class EditProfileFragment : Fragment(), HasCustomTitle, HasCustomAction {
         val nickname = binding.etNickname.text.toString().trim()
         profile.nickname = nickname
         binding.etNickname.error = if (nickname.length > 15) "Nickname is too long" else null
-        binding.tvNicknameSize.text = getString(R.string.char_сount_tv, nickname.length)
+        binding.tvNicknameSize.text = getString(R.string.char_count_tv, nickname.length)
         setAvatar()
     }
 
